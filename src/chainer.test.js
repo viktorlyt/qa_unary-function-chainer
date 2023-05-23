7ac1d166d30596977d1e8d71dc4ff350eb15bee1
viktorlyt <viktorlyt@gmail.com>
2023-05-24 00:28:04 +0300

    done

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,19 @@
-const { chainer } = require('./chainer');
+'use strict';
+
+describe('chainer', () => {
+  const { chainer } = require('./chainer');
+
+  it('should ', () => {
+    const a = jest.fn();
+    const b = jest.fn(a);
+    const c = jest.fn(b);
+    const d = jest.fn(c);
+
+    chainer([a, b, c, d])(0);
+
+    expect(a).toHaveBeenCalledWith(0);
+    expect(b).toHaveBeenCalledWith(a(0));
+    expect(c).toHaveBeenCalledWith(b(a(0)));
+    expect(d).toHaveBeenCalledWith(c(b(a(0))));
+  });
+});
